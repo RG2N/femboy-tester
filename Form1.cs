b715fab3bd1e152cc77ac450b3d1e1bee9c331d3
RG2N <64289295+RG2N@users.noreply.github.com>
2023-07-22 11:12:23 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace femboytester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Thread(() =>
+            {
+                //Thread.CurrentThread.IsBackground = true;
+                using (WebClient client = new WebClient())
+                {
+                    string downloadString = client.DownloadString("https://api.rghosting.xyz/femboy.php?name=" + textBox1.Text.ToLower());
+                    SetText("Result: " + downloadString + "%");
+                }
+            }).Start();
+        }
+
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.textBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.label2.Text = text;
+            }
+        }
+    }
+}
